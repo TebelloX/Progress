03e24bbc578cc8894360b8388ce726d1177efa9c
TebelloX <tebellonamo@gmail.com>
2021-01-21 14:13:34 +0200

    progress steps

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 const progress = document.getElementById("progress");
 const prev = document.getElementById("prev");
 const next = document.getElementById("next");
-const circle = document.querySelectorAll(".circle");
+const circles = document.querySelectorAll(".circle");
+
+let currentActive = 1;
+
+next.addEventListener("click", () => {
+  curActive++;
+
+  console.log(currentActive);
+});
